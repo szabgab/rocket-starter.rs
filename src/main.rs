df95cebc49d4cc25e5839ed421815318cf5f2bc1
Gabor Szabo <gabor@szabgab.com>
2024-05-08 18:51:53 +0300

    separating the header and the footer for the Tera templating system

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,8 @@ fn main() {
         let main_rs_template = include_str!("../data/tera/src/main.rs");
         let tests_rs_template = include_str!("../data/tera/src/tests.rs");
         let templates_index_html = include_str!("../data/tera/templates/index.html.tera");
+        let templates_incl_header_html = include_str!("../data/tera/templates/incl/header.html.tera");
+        let templates_incl_footer_html = include_str!("../data/tera/templates/incl/footer.html.tera");
 
         let cargo_toml = cargo_toml_template.replace("NAME", &args.name);
 
@@ -52,8 +54,10 @@ fn main() {
         std::fs::write(format!("{}/src/main.rs", &args.name), main_rs_template).unwrap();
         std::fs::write(format!("{}/src/tests.rs", &args.name), tests_rs_template).unwrap();
 
-        std::fs::create_dir_all(format!("{}/templates", &args.name)).unwrap();
+        std::fs::create_dir_all(format!("{}/templates/incl", &args.name)).unwrap();
         std::fs::write(format!("{}/templates/index.html.tera", &args.name), templates_index_html).unwrap();
+        std::fs::write(format!("{}/templates/incl/header.html.tera", &args.name), templates_incl_header_html).unwrap();
+        std::fs::write(format!("{}/templates/incl/footer.html.tera", &args.name), templates_incl_footer_html).unwrap();
 
         return;
     }
